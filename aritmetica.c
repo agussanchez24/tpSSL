719284fa0f806022982bb57202f5943b8d19b80c
juan lopez korach <50777759+juanse-lk@users.noreply.github.com>
2020-11-05 00:38:38 -0300

    revisar linea 100

--- aritmetica.c
+++ aritmetica.c
@@ -12,19 +12,22 @@ typedef struct {
 } numeroAstronomico;
 
 numeroAstronomico suma(numeroAstronomico, numeroAstronomico);
-char agregarCerosIzq(char *,int);
+bool esMenor(numeroAstronomico,numeroAstronomico);
+bool sonIguales(numeroAstronomico,numeroAstronomico);
+char* agregarCerosIzq(char *,int);
 
-char agregarCerosIzq(char *cadena,int cant){
-    if(cant == 0){
-        return(cadena);
+char* agregarCerosIzq(char *cadena,int cant){
+    if(cant != 0){
+        //char *numCeros;
+        char *numCeros = (char *) malloc(101 * sizeof(char));
+        //numCeros = "";
+        for(int i=cant;i>0;i--){
+        strcat(numCeros,"0");
+        }
+        strcat(numCeros,cadena);
+        //printf("%s\n",numCeros);
+        return(numCeros);
     }
-    char *numCeros;
-    numCeros = (char *) malloc(101 * sizeof(char));
-    for(int i=cant;i>0;i--){
-    strcat(numCeros,"0");
-    }
-    strcat(numCeros,cadena);
-    return(numCeros);
 }
 
 
@@ -46,30 +49,21 @@ numeroAstronomico suma(numeroAstronomico numeroAstronomicoUno, numeroAstronomico
     int i = 0;
     int lenUno = strlen(numUno);
     int lenDos = strlen(numDos);
+    int dif = abs(lenUno - lenDos);
     if(lenUno >= lenDos){  //aca falta arreglar. para que funcione bien los dos numeros tienen que quedar parejos (ej: 0923 y 0003) 
-        i = lenUno-1;
-        int dif = lenUno - lenDos;
-        agregarCerosIzq(numDos,dif+1);
-	    agregarCerosIzq(numUno,1);
-    }else{
-        i = lenDos-1;
-        int dif = lenDos - lenUno;
-        agregarCerosIzq(numUno,dif+1);
-        agregarCerosIzq(numDos,1);
-        }
         
+        numDos = agregarCerosIzq(numDos,dif+1);
+	    numUno = agregarCerosIzq(numUno,1);
+    }else{
+        
+        numUno = agregarCerosIzq(numUno,dif+1);
+        numDos = agregarCerosIzq(numDos,1);
+        }
+    i = strlen(numUno)-1;//ahora las dos longitudes son son iguales
     for(i;i>=0;i--){ 
 
         int num1 = numUno[i] - '0';
         int num2 = numDos[i] - '0';
-
-        if(!isdigit(numUno[i])){
-            num1 = 0;
-        }
-        if(!isdigit(numDos[i])){
-            num2 = 0;
-        }
-    
 
         if(carry == '0'){
             
@@ -77,9 +71,10 @@ numeroAstronomico suma(numeroAstronomico numeroAstronomicoUno, numeroAstronomico
             char carryTemp[1];
             char resultado[2];
             if(res >= 10){
-                res = res/10;
-                int carryNum = (num1 + num2)%10;
-                carry = 48 + carryNum; 
+                res = res%10;
+                int carryNum = (num1 + num2)/10;
+                carry =  carryNum + '0' ; 
+            
             }
             
             char *numInv;
@@ -99,10 +94,11 @@ numeroAstronomico suma(numeroAstronomico numeroAstronomicoUno, numeroAstronomico
             char carryTemp[1];
             char resultado[2];
             if(res >= 10){
-                res = res/10;
-                int carryNum = (num1 + num2)%10;
-                carry = 48 + carryNum; 
-                
+                res = res%10;
+                int carryNum = (num1 + num2 + carryNum)/10;
+                printf("%d\n",carryNum);
+                carry =  carryNum + 48; 
+                printf("%c\n",carry);
             }else{
                 carry = '0';
             }
@@ -118,19 +114,80 @@ numeroAstronomico suma(numeroAstronomico numeroAstronomicoUno, numeroAstronomico
         }
        
     }
-    // resultadoAstronomico.entero = resultadoAstro;
-    // return(resultadoAstronomico);
-    printf("%s\n",resultadoAstro);
+    resultadoAstronomico.entero = resultadoAstro;
+    return(resultadoAstronomico);
+    //printf("%s\n",resultadoAstro);
     
+}
+
+bool esMenor(numeroAstronomico numeroAstronomicoUno, numeroAstronomico numeroAstronomicoDos){
+    
+    char *numUno;
+    numUno = (char *) malloc(101 * sizeof(char));
+    numUno = numeroAstronomicoUno.entero;
+
+    char *numDos;
+    numDos = (char *) malloc(101 * sizeof(char));
+    numDos = numeroAstronomicoDos.entero;
+
+    int lenUno = strlen(numUno);
+    int lenDos = strlen(numDos);
+
+    if(lenUno==lenDos){
+        while(*numUno != '\0'){
+
+            int num1 = *numUno - '0';
+            int num2 = *numDos - '0';
+            if(num1!=num2){
+                return(num1<num2);
+            }
+            numDos++;
+            numUno++;
+        }
+    }else{
+        return(lenUno<lenDos);
+    }
+}
+
+bool sonIguales(numeroAstronomico numeroAstronomicoUno, numeroAstronomico numeroAstronomicoDos){
+
+char *numUno;
+    numUno = (char *) malloc(101 * sizeof(char));
+    numUno = numeroAstronomicoUno.entero;
+
+    char *numDos;
+    numDos = (char *) malloc(101 * sizeof(char));
+    numDos = numeroAstronomicoDos.entero;
+
+    int lenUno = strlen(numUno);
+    int lenDos = strlen(numDos);
+
+    if(lenUno==lenDos){
+        while(*numUno != '\0'){
+
+            if(*numUno != *numDos){
+                return(false);
+            }
+            numDos++;
+            numUno++;
+        }return(true);
+    }else{
+        return(false);
+    }
 }
 
 
 int main(){
-    
+
     numeroAstronomico astroUno;
     numeroAstronomico astroDos;
-    astroUno.entero = "020";
-    astroDos.entero = "900";
-    suma(astroUno,astroDos);
-    return 0;
+    numeroAstronomico astroRes;
+    astroUno.entero = "10";
+    astroDos.entero = "3";
+    bool res = esMenor(astroUno,astroDos); //OK
+    bool sonIng = sonIguales(astroUno,astroDos); //Ok
+    astroRes = suma(astroUno,astroDos); //Ok
+    
+    printf("%s\n", astroRes.entero);
+    return(0);
 }
